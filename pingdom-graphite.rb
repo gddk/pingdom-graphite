2c7828076379be119e3caaa8ae32ca1b60238f24
Dave <dkoopman@godaddy.com>
2014-05-12 08:07:42 -0700

    better looping

--- pingdom-graphite.rb
+++ pingdom-graphite.rb
@@ -11,32 +11,38 @@ if ( !@checkid || @checkid <= 0 )
 	exit 1
 end
 
+now = Time.now.to_i
+# @from is now - 2 days
+@from = now - 86400 * 2
+@to = now
+
+puts "@from = #{Time.at(@from).strftime("%Y-%m-%d %H:%M:%S")}, @to=#{Time.at(@to).strftime("%Y-%m-%d %H:%M:%S")}"
+
 # FIRST GET THE LIST OF PROBES - WILL NEED THEM LATER
+puts "Pingdom.probes() start"
 @probes = {}
 temp = Pingdom.probes()
 temp['probes'].each do |probe|
 	@probes[probe['id']] = probe
 end
-
-@halfdays = 60
-@downTo = 3
-@now = Time.now
+puts "Pingdom.probes() finish"
 
 @graphite = Graphite.new
 # Hammer on the Pingdom API with 5 threads to get done 5x as fast
 wq = WorkQueue.new 5
-@halfdays.downto(@downTo).each do |halfday|
+# step up in 12 hour increments due to the 1000 limit on the Pingdom API
+(@from..@to).step(86400/2) do |from|
 	wq.enqueue_b {
-		from = @now - 43200 * halfday
-		to = from + 43200 -1
-		puts "From #{from.strftime("%Y-%m-%d %H:%M:%S")} to #{to.strftime("%Y-%m-%d %H:%M:%S")}"
-		results = Pingdom.results(@checkid, from, to)
+		# pTo is pFrom + 12 hours - 1 second
+		pFrom = from
+		pTo = pFrom + 86400/2 - 1
+		puts "pFrom #{Time.at(pFrom).strftime("%Y-%m-%d %H:%M:%S")} pTo #{Time.at(pTo).strftime("%Y-%m-%d %H:%M:%S")}"
+		results = Pingdom.results(@checkid, Time.at(pFrom), Time.at(pTo))
 #puts results
 		results and results["results"] and results["results"].each do |result|
 			country = @probes[result['probeid']]['country'].gsub(/[^A-Za-z0-9]/, "_")
 			city = @probes[result['probeid']]['city'].gsub(/[^A-Za-z0-9]/, "_")
 			if result['status'] == "up"
-				#puts "@graphite.report(\"pingdom.#{@checkid}.rollup.responsetime\", \"#{result['responsetime'].to_f}\", \"#{result['time'].to_i}\")"
 				@graphite.report("pingdom.#{@checkid}.rollup.responsetime", "#{result['responsetime'].to_f}", "#{result['time'].to_i}")
 				@graphite.report("pingdom.#{@checkid}.country.#{country}.responsetime", "#{result['responsetime'].to_f}", "#{result['time']}")
 				@graphite.report("pingdom.#{@checkid}.city.#{country}.#{city}.responsetime", "#{result['responsetime'].to_f}", "#{result['time'].to_i}")
@@ -51,5 +57,4 @@ wq = WorkQueue.new 5
 end
 wq.join
 @graphite.close_socket
-
 
